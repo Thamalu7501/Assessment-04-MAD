9b2e560fa279946dbfcde8d5b6f19c413545213f
Thamalu Amarasingha <thamalu7501@gmail.com>
2023-11-30 18:39:01 +0530

    UI design

---
+++ Weather App/app/src/main/java/com/example/weatherapp/Forecast.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Forecast : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast)
+
+    }
+}
---
+++ Weather App/app/src/main/java/com/example/weatherapp/OtherCities.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class OtherCities :  ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_othercities)
+
+    }
+}
